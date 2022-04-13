fae2473a8e2d46dceef20f28a9539f9d8440b466
Boule2Papier <victor.quesnay@edu.devinci.fr>
2022-04-13 17:02:56 +0200

    Update sandbox.js

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,13 +1,19 @@
-/* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
-
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+const dedicatedbrand = require('./sites/dedicatedbrand');
+const montlimart = require('./sites/montlimart');
+const adresseparis = require('./sites/adresseparis');
+async function sandbox (eshop) {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    products1 = await dedicatedbrand.scrape('https://www.dedicatedbrand.com/en/men/news');
+    products2 = await montlimart.scrape('https://www.montlimart.com/toute-la-collection.html?limit=all');
+    products3 = await adresseparis.scrape('https://adresse.paris/630-toute-la-collection');
+    products1=products1.concat(products2)
+    products1=products1.concat(products3)
+    console.log(products1)
+    
+    saveToFile(products1)
 
-    console.log(products);
     console.log('done');
     process.exit(0);
   } catch (e) {
@@ -15,7 +21,18 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
     process.exit(1);
   }
 }
-
 const [,, eshop] = process.argv;
+sandbox()
 
-sandbox(eshop);
+function saveToFile(products){
+  const fs=require('fs');
+  const json=JSON.stringify(products);
+
+
+  fs.writeFileSync("./all_products.json",json,function(err, result) {
+   if(err) console.log('error', err);
+ });
+}
+
+
+
